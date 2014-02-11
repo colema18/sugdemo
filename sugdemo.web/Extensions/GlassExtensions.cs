a1634ece30793fb9405feebc05b13fa8b89eab7b
James Coleman <colema18@gmail.com>
2014-02-11 12:50:28 -0600

    Reading Listing now links to each Reading

---
+++ sugdemo.web/Extensions/GlassExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Links;
+using sugdemo.models;
+
+namespace sugdemo.web.Extensions
+{
+    public static class GlassExtensions
+    {
+        public static string Link(this IPage page)
+        {
+            return LinkManager.GetItemUrl(Sitecore.Context.Database.GetItem(page.Id.ToString()));
+        }
+    }
+}
